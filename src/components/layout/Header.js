818df9c6ea6e092f040f4703cdd643ae97d5c9dc
Ugur Yilmaz <uyilmaz1@binghamton.edu>
2018-09-01 00:38:27 -0400

    browser fix

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,9 +7,7 @@ const Header = props => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="" className="navbar-brand">
-          {branding}
-        </a>
+        {branding}
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
